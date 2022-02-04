21b3ed8b8fcf3bf8c1de53c0d233e1ea026450d5
C1324634 Ricardo Carteri Uehlein <c1324634@bb.com>
2022-02-04 10:54:41 -0300

    Aula 04: Introdução ao TypeScript para Angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,14 @@
+var minhaVar  = 'minha variavel';
+
+function minhaFunction(x, y) { return x + y; }
+
+let num = 2;
+const PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (n) { return n * 2;})
+
+numeros.map(n => { n * 2;})
+
+class Matematica {
+    soma(x, y) { return x + y; }
+}
